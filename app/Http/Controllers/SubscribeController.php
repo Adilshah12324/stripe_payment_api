43c9d239cb3f69e98ab767cb6a6700ee75dbd638
adilkhan12324 <adilkhan12324@gmail.com>
2023-05-25 00:57:52 +0500

    event and listener for user email

---
+++ app/Http/Controllers/SubscribeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\SendMail;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
+
+class SubscribeController extends Controller
+{
+    public function subscribed(){
+        Event::dispatch(new SendMail(1));
+        dd('asd');
+    }
+}
